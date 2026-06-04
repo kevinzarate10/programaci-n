fdfa310b87758d7d4a7b2d76a84b9f8d1019a410
kevinzarate10 <kevin.zarate@comunidad.ub.edu.ar>
2026-06-04 01:09:48 -0300

    Delete modulos.1.py

--- modulos.1.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-def calcular_area_circulo(radio):
-    return pi * (radio ** 2)
-entrada_radio = float(input("ingresa el radio del círculo: "))
-area_resultado = calcular_area_circulo(entrada_radio)
-print(f"el área del círculo con radio {entrada_radio} es: {area_resultado}")
